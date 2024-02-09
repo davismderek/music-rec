fa648993c0305e432650a4fce7cb79676d46f6ef
Derek Davis <davismderek@gmail.com>
2024-02-09 16:14:31 -0500

    js updates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 
+// ********THIS IS FOR ADDING SONGS TO PLAYLIST ON MyPLAYLISTS PAGE
 document.addEventListener("DOMContentLoaded", function () {
     const songsForm = document.querySelector("#songs");
 
-    songsForm.addEventListener("submit", function (event) {
+    songsForm.addEventListener("submit", (event) => {
         event.preventDefault();
         const songInput = this.querySelector('input[name="songEntry"]');
         generateNewText();
         generateList(songInput);
     });
 
-    function generateNewText() {
-        const newTextContainer = document.getElementById('newSongsText');
-        newTextContainer.style.display = 'block';
+    const generateNewText = () => {
+        const newTextContainer = document.getElementById("newSongsText");
+        newTextContainer.style.display = "block";
         newTextContainer.innerHTML = "<p>New Playlist</p>";
-    }
+    };
 
-    function generateList(songInput) {
-        const songList = document.querySelector('#songList');
-        const songElement = document.createElement('li');
-        songElement.textContent = songInput.value
+    const generateList = (songInput) => {
+        const songList = document.querySelector("#songList");
+        const songElement = document.createElement("li");
+        songElement.textContent = songInput.value;
         songList.appendChild(songElement);
         clearInput(songInput);
-    }
+    };
 
-    function clearInput(input) {
-        input.value = '';
-        return;
-    }
-})
+    const clearInput = (input) => {
+        input.value = "";
+    };
+});
